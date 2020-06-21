a9ef2ec8ffd67e016b72c50ce5a73af22c382a00
egeozsoy <egeozsoy97@gmail.com>
2020-06-21 13:14:30 +0200

    - begin model cleanup

--- code/osvos/model.py
+++ code/osvos/model.py
@@ -10,203 +10,17 @@ from helpers.model import SegmentationModel
 from torchvision.models.detection.image_list import ImageList
 
 
-def get_model_instance_segmentation(num_classes):
-    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-    # get number of input features for the classifier
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # replace the pre-trained head with a new one
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-    # now get the number of input features for the mask classifier
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    hidden_layer = 256
-    # and replace the mask predictor with a new one
-    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
-                                                       hidden_layer,
-                                                       num_classes)
-
-    return model
-
-
-class SlowFastLayers(nn.Module):
-    def __init__(self, input_size, device, slow_pathway_size, fast_pathway_size):
-        super(SlowFastLayers, self).__init__()
-        self.device = device
-        self.slow_pathway_size = slow_pathway_size
-        self.fast_pathway_size = fast_pathway_size
-
-        kernel_size_slow1, kernel_size_slow2, kernel_size_slow3 = self._calc_kernel_sizes(self.slow_pathway_size)
-        kernel_size_fast1, kernel_size_fast2, kernel_size_fast3 = self._calc_kernel_sizes(self.fast_pathway_size)
-
-        kernel_size_f2s1, slow_out1, fast_out1 = self._calc_fuse_kernel_size(slow_in=self.slow_pathway_size,
-                                                                             slow_kernel=kernel_size_slow1,
-                                                                             fast_in=self.fast_pathway_size,
-                                                                             fast_kernel=kernel_size_fast1)
-        kernel_size_f2s2, _, _ = self._calc_fuse_kernel_size(slow_in=slow_out1, slow_kernel=kernel_size_slow2,
-                                                             fast_in=fast_out1, fast_kernel=kernel_size_fast2)
-
-        self.fast_conv1, self.bn_f1 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_fast1,
-                                                             in_channels=input_size, out_channels=32)
-
-        self.slow_conv1, self.bn_s1 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_slow1,
-                                                             in_channels=input_size, out_channels=192)
-
-        self.fast_conv2, self.bn_f2 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_fast2,
-                                                             in_channels=32, out_channels=32)
-
-        self.slow_conv2, self.bn_s2 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_slow2,
-                                                             in_channels=256, out_channels=192)
-
-        self.fast_conv3, self.bn_f3 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_fast3,
-                                                             in_channels=32, out_channels=32)
-
-        self.slow_conv3, self.bn_s3 = self._init_conv_and_bn(temporal_kernelsize=kernel_size_slow3,
-                                                             in_channels=256, out_channels=224)
-
-        self.conv_f2s1, self.bn_f2s1 = self._init_fuse_and_bn(kernel_size_f2s1)
-
-        self.conv_f2s2, self.bn_f2s2 = self._init_fuse_and_bn(kernel_size_f2s2)
-
-        self.relu = nn.ReLU(inplace=True)
-
-    def _init_conv_and_bn(self, temporal_kernelsize, in_channels, out_channels):
-        conv = nn.Conv3d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=(temporal_kernelsize, 3, 3),
-            padding=(0, 1, 1))
-
-        bn = nn.BatchNorm3d(out_channels)
-
-        return conv, bn
-
-    def _init_fuse_and_bn(self, temporal_kernelsize):
-        conv_f2s = nn.Conv3d(
-            32,
-            64,
-            kernel_size=[temporal_kernelsize, 1, 1],
-            stride=[1, 1, 1],
-            padding=[0, 0, 0],
-            bias=False,
-        )
-
-        bn_f2s = nn.BatchNorm3d(64)
-
-        return conv_f2s, bn_f2s
-
-    def _calc_kernel_sizes(self, pathway_size):
-        div = pathway_size // 3
-        if pathway_size % 3 == 0:
-            return (div, div + 1, div + 1)
-        elif pathway_size % 3 == 1:
-            return (div + 1, div + 1, div + 1)
-        elif pathway_size % 3 == 2:
-            return (div + 1, div + 1, div + 2)
-
-    def _calc_fuse_kernel_size(self, slow_in, slow_kernel, fast_in, fast_kernel):
-        out_slow = (slow_in - slow_kernel) + 1
-        out_fast = (fast_in - fast_kernel) + 1
-        fuse_kernel_size = out_fast - out_slow + 1
-        return fuse_kernel_size, out_slow, out_fast
-
-    def fuse(self, slow, fast, conv, bn):
-        fuse = conv(fast)
-        fuse = bn(fuse)
-        fuse = self.relu(fuse)
-        x_s_fuse = torch.cat([slow, fuse], 1)
-        return x_s_fuse, fast
-
-    def forward(self, slow, fast):
-        # First Conv Layer
-        slow = self.slow_conv1(slow)
-        slow = self.bn_s1(slow)
-        slow = self.relu(slow)
-
-        fast = self.fast_conv1(fast)
-        fast = self.bn_f1(fast)
-        fast = self.relu(fast)
-
-        # Fuse1
-        slow, fast = self.fuse(slow, fast, self.conv_f2s1, self.bn_f2s1)
-
-        # Second Conv Layer
-        slow = self.slow_conv2(slow)
-        slow = self.bn_s2(slow)
-        slow = self.relu(slow)
-
-        fast = self.fast_conv2(fast)
-        fast = self.bn_f2(fast)
-        fast = self.relu(fast)
-
-        # Fuse2
-        slow, fast = self.fuse(slow, fast, self.conv_f2s2, self.bn_f2s2)
-
-        # Second Conv Layer
-        slow = self.slow_conv3(slow)
-        slow = self.bn_s3(slow)
-
-        fast = self.fast_conv3(fast)
-        fast = self.bn_f3(fast)
-        return slow, fast
-
-    def temporally_enhance_features(self, slow_features, fast_features):
-        # List of dicts to dict of lists
-        slow_features = {k: [dic[k] for dic in slow_features] for k in slow_features[0]}
-        fast_features = {k: [dic[k] for dic in fast_features] for k in fast_features[0]}
-        merged_features = OrderedDict()
-        for key in slow_features.keys():
-            key_scale_slow_features = torch.stack(slow_features[key]).to(self.device).transpose(1, 2)
-            key_scale_fast_features = torch.stack(fast_features[key]).to(self.device).transpose(1, 2)
-            key_scale_slow_features, key_scale_fast_features = self.forward(key_scale_slow_features,
-                                                                            key_scale_fast_features)
-
-            merged_features[key] = torch.cat([key_scale_slow_features, key_scale_fast_features], dim=1).squeeze(dim=2)
-            del key_scale_slow_features, key_scale_fast_features
-
-        return merged_features
-
-
 class OsvosSegmentationModel(SegmentationModel):
-    def __init__(self, device, slow_pathway_size, fast_pathway_size):
-        super(OsvosSegmentationModel, self).__init__()
-        # TODO continue here
-        self.device = device
-        self.maskrcnn_model = get_model_instance_segmentation(num_classes=2)
-        self.maskrcnn_model.load_state_dict(torch.load('maskrcnn/maskrcnn_model.pth'))
+    def __init__(self, device, slow_pathway_size, fast_pathway_size, batchsize=8):
+        super(OsvosSegmentationModel, self).__init__(device, slow_pathway_size, fast_pathway_size)
 
         # Freeze most of the weights
         for param in self.maskrcnn_model.backbone.parameters():
-            param.requires_grad = False
+            param.requires_grad = True
         for param in self.maskrcnn_model.rpn.parameters():
-            param.requires_grad = False
-
-        self.slow_pathway_size = slow_pathway_size
-        self.fast_pathway_size = fast_pathway_size
-
-        self.slow_fast = SlowFastLayers(256, device=device, slow_pathway_size=slow_pathway_size,
-                                        fast_pathway_size=fast_pathway_size)
-
-        self.bs = batch_size
-        self.maskrcnn_bs = maskrcnn_batch_size
+            param.requires_grad = True
+        self.bs = batchsize
         self.maskrcnn_model.roi_heads.detections_per_img = 10
-
-    @torch.no_grad()
-    def compute_maskrcnn_features(self, images_tensors):
-        self.maskrcnn_model.eval()
-        all_features = OrderedDict()
-        # Extract feature from respective extractor
-        for i in range(ceil(len(images_tensors) / self.maskrcnn_bs)):
-            batch_imgs = images_tensors[i * self.maskrcnn_bs:(i + 1) * self.maskrcnn_bs].to(self.device)
-            batch_features = self.maskrcnn_model.backbone(batch_imgs)
-            for key, value in batch_features.items():
-                if key not in all_features:
-                    all_features[key] = value.cpu()
-                else:
-                    all_features[key] = torch.cat([all_features[key], value.cpu()])
-
-        if self.training:
-            self.maskrcnn_model.train()
-        return all_features
 
     def batch_slice_features(self, features: OrderedDict, begin, end):
         batch_features = OrderedDict()
@@ -214,21 +28,6 @@ class OsvosSegmentationModel(SegmentationModel):
             batch_features[key] = value[begin:end].to(self.device)
 
         return batch_features
-
-    @torch.no_grad()
-    def compute_rpn_proposals(self, images, features):
-
-        self.maskrcnn_model.rpn.eval()
-        all_proposals = []
-        bs = 4
-        for i in range(ceil(len(images.tensors) / bs)):
-            batch_imgs = ImageList(images.tensors[i * bs:(i + 1) * bs].to(self.device),
-                                   images.image_sizes[i * bs:(i + 1) * bs])
-            batch_features = self.batch_slice_features(features, begin=i * bs, end=(i + 1) * bs)
-            batch_proposals, _ = self.maskrcnn_model.rpn(batch_imgs, batch_features, None)
-            all_proposals.extend(batch_proposals)
-
-        return all_proposals
 
     def _slice_features(self, features: OrderedDict, image_feature_idx, pathway_size):
         batch_features = OrderedDict()
@@ -244,18 +43,6 @@ class OsvosSegmentationModel(SegmentationModel):
                 targets[i][key] = value.to(device)
 
     '''Padding at beginning and end of the sequence (we do not have real neighbouring feature maps there)'''
-
-    def apply_padding(self, image_features):
-        padding_count = self.fast_pathway_size // 2
-        for key, image_feature in image_features.items():
-            image_features[key] = torch.cat(
-                [torch.zeros_like(image_feature[:1, :, :, :].repeat(padding_count, 1, 1, 1)), image_feature])
-
-        for key, image_feature in image_features.items():
-            image_features[key] = torch.cat(
-                [image_feature, torch.zeros_like(image_feature[:1, :, :, :].repeat(padding_count, 1, 1, 1))])
-
-        return image_features
 
     def forward(self, images, targets=None, optimizer=None):
         original_image_sizes = []

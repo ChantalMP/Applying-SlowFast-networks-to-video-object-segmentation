a787da3dbc7abbc7eb059aeb65928e6f6271304c
egeozsoy <egeozsoy97@gmail.com>
2020-07-04 09:16:27 +0200

    - changed config

--- code/helpers/constants.py
+++ code/helpers/constants.py
@@ -2,13 +2,13 @@ from pathlib import Path
 
 import shutil
 
-environment = 'colab'  # or colab
+environment = 'local'  # or colab
 print(f'Environment is {environment}')
-slow_pathway_size = 7
-fast_pathway_size = 7
+slow_pathway_size = 1
+fast_pathway_size = 1
 continue_training = False
-use_caching = False
-model_name = f'model_maskrcnn_slowfast_sp_{slow_pathway_size}fp_{fast_pathway_size}'
+use_caching = False  # At least once trained, turning this on is probably a good idea (make sure it still loads the correct model)
+model_name = f'model_maskrcnn_slowfast_sp_{slow_pathway_size}fp_{fast_pathway_size}_{use_caching}'
 random_seed = 63
 root_dir_path = Path('/content/gdrive/My Drive/Python Projects/adl4cv_root') if environment == 'colab' else Path('')
 writer_dir = (root_dir_path / 'runs') / model_name
